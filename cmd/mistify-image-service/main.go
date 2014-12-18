6e41a2196c4685b7ce177d20988135f8afaee67a
yonishostak <yoni@omniti.com>
2014-12-18 15:05:16 -0500

    MIST-228 actually start the service

--- cmd/mistify-image-service/main.go
+++ cmd/mistify-image-service/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	flag "github.com/docker/docker/pkg/mflag"
-	"os"
-    
+    flag "github.com/docker/docker/pkg/mflag"
+    "os"
+    "github.com/mistifyio/mistify-agent/log"
     "github.com/mistifyio/mistify-image-service"
+    "strings"
 )
 
 const (
@@ -12,19 +13,32 @@ const (
 )
 
 func main() {
-	var help bool
+    var help bool
+    var configFile string
 
-	flag.BoolVar(&help, []string{"h", "#help", "-help"}, false, "display help")
+    flag.BoolVar(&help, []string{"h", "#help", "-help"}, false, "display help")
     flag.StringVar(&configFile, []string{"c", "#config-file", "-config-file"}, DEFAULT_CONFIG_FILE, "configuration file")
 
-	flag.Parse()
+    flag.Parse()
 
-	if help {
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-    
-    context := imageservice.NewContext()
-    
-    imageservice.Run(context, "127.0.0.1")
+    if help {
+        flag.PrintDefaults()
+        os.Exit(0)
+    }
+
+    config, err := imageservice.ConfigFromFile(configFile)
+    if nil != err {
+        log.Fatal(err)
+    }
+
+    context, err := imageservice.NewContext(config)
+    if nil != err {
+        log.Fatal(err)
+    }
+
+    err = imageservice.Run(context, strings.Join([]string{config.Address, config.Port}, ":"))
+    if nil != err {
+        log.Fatal(err)
+    }
+
 }
