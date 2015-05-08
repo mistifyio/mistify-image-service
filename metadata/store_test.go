869cea6ab9e815845d28dccf74a9350aff1d5b69
Brian Bickerton <bbickerton@omniti.com>
2015-05-08 19:55:03 +0000

    Add tests for metadata.Store

---
+++ metadata/store_test.go
@@ -0,0 +1,53 @@
+package metadata_test
+
+import (
+	"os"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/mistifyio/mistify-image-service/metadata"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockStore struct{}
+
+func (ms *MockStore) Init(i interface{}) error                      { return nil }
+func (ms *MockStore) Shutdown() error                               { return nil }
+func (ms *MockStore) List(s string) ([]*metadata.Image, error)      { return []*metadata.Image{}, nil }
+func (ms *MockStore) GetByID(s string) (*metadata.Image, error)     { return &metadata.Image{}, nil }
+func (ms *MockStore) GetBySource(s string) (*metadata.Image, error) { return &metadata.Image{}, nil }
+func (ms *MockStore) Put(i *metadata.Image) error                   { return nil }
+func (ms *MockStore) Delete(s string) error                         { return nil }
+
+func TestMain(m *testing.M) {
+	log.SetLevel(log.WarnLevel)
+
+	os.Exit(m.Run())
+}
+
+func TestList(t *testing.T) {
+	list := metadata.List()
+	assert.NotNil(t, list)
+}
+
+func TestNewStore(t *testing.T) {
+	list := metadata.List()
+	if len(list) == 0 {
+		return
+	}
+
+	assert.NotNil(t, metadata.NewStore(list[0]))
+	assert.Nil(t, metadata.NewStore("qweryasdf"))
+}
+
+func TestRegister(t *testing.T) {
+	registerMockStore()
+
+	assert.NotNil(t, metadata.NewStore("mock"))
+}
+
+func registerMockStore() {
+	metadata.Register("mock", func() metadata.Store {
+		return &MockStore{}
+	})
+}
