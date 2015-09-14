f9b87a81731ff5ff5f6171ec1dafa9800e0ab87a
Brian Bickerton <brian.bickerton@gmail.com>
2015-09-15 05:36:35 +1000

    Use test suites for fetcher and context

--- context_test.go
+++ context_test.go
@@ -2,98 +2,68 @@ package imageservice_test
 
 import (
 	"encoding/json"
-	"os"
-	"testing"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/mistifyio/mistify-image-service"
 	"github.com/mistifyio/mistify-image-service/images"
+	imocks "github.com/mistifyio/mistify-image-service/images/mocks"
 	"github.com/mistifyio/mistify-image-service/metadata"
+	mmocks "github.com/mistifyio/mistify-image-service/metadata/mocks"
 	"github.com/spf13/viper"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-var ctx *imageservice.Context
+type ContextTestSuite struct {
+	suite.Suite
+}
 
-func TestMain(m *testing.M) {
+func (s *ContextTestSuite) SetupSuite() {
 	log.SetLevel(log.FatalLevel)
 
-	imgDir := "/tmp/test_image_service"
-	mdFile := "/tmp/test_image_service.db"
-
-	// Clean up
-	if err := cleanup(imgDir, mdFile); err != nil {
-		log.Fatal("failed to clean up")
-	}
-
-	// Set up config
-	viper.SetDefault("imageStoreType", "fs")
-	viper.SetDefault("imageStoreConfig", &images.FSConfig{
-		Dir: imgDir,
+	// Images Store Setup
+	images.Register("mock", func() images.Store {
+		return &imocks.Store{}
 	})
+	viper.SetDefault("imageStoreType", "mock")
+	viper.SetDefault("imageStoreConfig", nil)
 
-	viper.SetDefault("metadataStoreType", "kvite")
-	viper.SetDefault("metadataStoreConfig", &metadata.KViteConfig{
-		Filename: mdFile,
-		Table:    "test_image_service",
+	// Metadata Store Setup
+	metadata.Register("mock", func() metadata.Store {
+		return &mmocks.Store{}
 	})
-
-	// Run the tests
-	code := m.Run()
-
-	// Clean up
-	if err := cleanup(imgDir, mdFile); err != nil {
-		log.Fatal("failed to clean up")
-	}
-
-	// Exit with test run exit code
-	os.Exit(code)
+	viper.SetDefault("metadataStoreType", "mock")
+	viper.SetDefault("metadataStoreConfig", nil)
 }
 
-func cleanup(imgDir, mdFile string) error {
-	if err := os.RemoveAll(imgDir); err != nil && !os.IsNotExist(err) {
-		log.Error(err)
-		return err
-	}
+func (s *ContextTestSuite) TestContextInitImageStore() {
+	context := &imageservice.Context{}
 
-	if err := os.Remove(mdFile); err != nil && !os.IsNotExist(err) {
-		log.Error(err)
-		return err
-	}
-
-	return nil
-}
-
-func TestContextInitImageStore(t *testing.T) {
-	ctx := &imageservice.Context{}
-
-	assert.Error(t, ctx.InitImageStore("asdfwqfas", nil))
-	assert.Nil(t, ctx.ImageStore)
+	s.Error(context.InitImageStore("asdfwqfas", nil))
+	s.Nil(context.ImageStore)
 
 	imageStoreType := viper.GetString("imageStoreType")
 	imageStoreConfig, _ := json.Marshal(viper.Get("imageStoreConfig"))
-	assert.NoError(t, ctx.InitImageStore(imageStoreType, imageStoreConfig))
-	assert.NotNil(t, ctx.ImageStore)
+	s.NoError(context.InitImageStore(imageStoreType, imageStoreConfig))
+	s.NotNil(context.ImageStore)
 }
 
-func TestContextNewMetadataStore(t *testing.T) {
-	ctx := &imageservice.Context{}
+func (s *ContextTestSuite) TestContextNewMetadataStore() {
+	context := &imageservice.Context{}
 
-	assert.Error(t, ctx.InitMetadataStore("asdfwqfas", nil))
-	assert.Nil(t, ctx.MetadataStore)
+	s.Error(context.InitMetadataStore("asdfwqfas", nil))
+	s.Nil(context.MetadataStore)
 
 	metadataStoreType := viper.GetString("metadataStoreType")
 	metadataStoreConfig, _ := json.Marshal(viper.Get("metadataStoreConfig"))
-	assert.NoError(t, ctx.InitMetadataStore(metadataStoreType, metadataStoreConfig))
-	assert.NotNil(t, ctx.MetadataStore)
+	s.NoError(context.InitMetadataStore(metadataStoreType, metadataStoreConfig))
+	s.NotNil(context.MetadataStore)
 }
 
-func TestNewContext(t *testing.T) {
+func (s *ContextTestSuite) TestNewContext() {
 	context, err := imageservice.NewContext()
-	assert.NoError(t, err)
-	assert.NotNil(t, context)
-	assert.NotNil(t, context.ImageStore)
-	assert.NotNil(t, context.MetadataStore)
-	assert.NotNil(t, context.Fetcher)
-	ctx = context
+	s.NoError(err)
+	s.NotNil(context)
+	s.NotNil(context.ImageStore)
+	s.NotNil(context.MetadataStore)
+	s.NotNil(context.Fetcher)
 }
--- fetcher_test.go
+++ fetcher_test.go
@@ -2,59 +2,123 @@ package imageservice_test
 
 import (
 	"bytes"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/mistifyio/mistify-image-service"
+	"github.com/mistifyio/mistify-image-service/images"
 	"github.com/mistifyio/mistify-image-service/metadata"
-	"github.com/stretchr/testify/assert"
+	"github.com/pborman/uuid"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/suite"
 )
 
-var mockImageData = []byte("testdatatestdatatestdata")
-
-func TestFetcherReceive(t *testing.T) {
-	req, err := http.NewRequest("GET", "http://localhost", bytes.NewReader(mockImageData))
-	assert.NoError(t, err)
-	req.Header.Add("X-Image-Type", "kvm")
-	req.Header.Add("X-Image-Comment", "test image")
-
-	image, err := ctx.Fetcher.Receive(req)
-	assert.NoError(t, err)
-	assert.NotNil(t, image)
-	assert.EqualValues(t, len(mockImageData), image.Size)
-	assert.Equal(t, "kvm", image.Type)
-	assert.Equal(t, "test image", image.Comment)
+type FetcherTestSuite struct {
+	suite.Suite
+	Context     *imageservice.Context
+	ImageData   []byte
+	FetchServer *httptest.Server
+	StoreDir    string
 }
 
-func TestFetcherFetch(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if _, err := w.Write(mockImageData); err != nil {
+func (s *FetcherTestSuite) SetupSuite() {
+	log.SetLevel(log.FatalLevel)
+
+	s.ImageData = []byte("testdatatestdatatestdata")
+
+	// Test Server to serve image data for fetching
+	s.FetchServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write(s.ImageData); err != nil {
 			log.WithField("error", err).Error("Failed to write mock image data to response")
 		}
 	}))
-	defer ts.Close()
+}
 
+func (s *FetcherTestSuite) SetupTest() {
+	// NOTE: Using the mocks here would require somewhat complicated logic,
+	// approaching a real in-memory Store. Might as well use actual stores
+	// (which are tested themselves in their respective packages)
+
+	s.StoreDir, _ = ioutil.TempDir("", "fetcherTest-"+uuid.New())
+	// Images Store Setup
+	imageStoreConfig := &images.FSConfig{
+		Dir: s.StoreDir,
+	}
+	viper.SetDefault("imageStoreType", "fs")
+	viper.SetDefault("imageStoreConfig", imageStoreConfig)
+
+	// Metadata Store Setup
+	metadataStoreConfig := &metadata.KViteConfig{
+		Filename: filepath.Join(s.StoreDir, "kvite.db"),
+		Table:    "test",
+	}
+	viper.SetDefault("metadataStoreType", "kvite")
+	viper.SetDefault("metadataStoreConfig", metadataStoreConfig)
+
+	// Set up context
+	ctx, err := imageservice.NewContext()
+	s.Require().NoError(err)
+	s.Context = ctx
+}
+
+func (s *FetcherTestSuite) TearDownTest() {
+	s.NoError(os.RemoveAll(s.StoreDir))
+}
+
+func (s *FetcherTestSuite) TearDownSuite() {
+	s.FetchServer.Close()
+}
+
+func TestFetcherTestSuite(t *testing.T) {
+	suite.Run(t, new(FetcherTestSuite))
+}
+
+func (s *FetcherTestSuite) TestFetcherReceive() {
+	imageType := "kvm"
+	imageComment := "test image"
+
+	// Create the upload request
+	req, _ := http.NewRequest("GET", "http://localhost", bytes.NewReader(s.ImageData))
+	req.Header.Add("X-Image-Type", "kvm")
+	req.Header.Add("X-Image-Comment", "test image")
+
+	image, err := s.Context.Fetcher.Receive(req)
+	s.NoError(err)
+	s.EqualValues(len(s.ImageData), image.Size, "sizes should match")
+	s.Equal(imageType, image.Type, "types should match")
+	s.Equal(imageComment, image.Comment, "comments should match")
+	s.NotEmpty(image.ID, "id should have been assigned")
+}
+
+func (s *FetcherTestSuite) TestFetcherFetch() {
 	image := &metadata.Image{
 		ID:     metadata.NewID(),
-		Source: ts.URL,
+		Source: s.FetchServer.URL,
 		Type:   "kvm",
 	}
 
-	image, err := ctx.Fetcher.Fetch(image)
-	assert.NoError(t, err)
-	assert.NotNil(t, image.ID)
-	for i := 0; i < 10; i++ {
-		image, err := ctx.MetadataStore.GetByID(image.ID)
-		assert.NoError(t, err)
+	image, err := s.Context.Fetcher.Fetch(image)
+	s.NoError(err)
+	s.NotNil(image.ID)
+	// Wait for the image to be completely fetched
+	for i := 0; i < 300; i++ {
+		image, err := s.Context.MetadataStore.GetByID(image.ID)
+		s.Require().NotNil(image, "image should not be nil")
+		s.NoError(err)
+
 		if image.Status == metadata.StatusComplete || image.Status == metadata.StatusError {
 			break
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
-	assert.Equal(t, metadata.StatusComplete, image.Status)
-	assert.Equal(t, "kvm", image.Type)
-	assert.Equal(t, ts.URL, image.Source)
-	assert.EqualValues(t, len(mockImageData), image.Size)
+	s.Equal(metadata.StatusComplete, image.Status)
+	s.Equal("kvm", image.Type)
+	s.Equal(s.FetchServer.URL, image.Source)
+	s.EqualValues(len(s.ImageData), image.Size)
 }
